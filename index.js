31bbd9479794bc805c01d1f65f41582bd36b9d5a
veeti kuusikko <veeti.kuusikko@gmail.com>
2024-08-14 13:04:55 +0300

    päivityksii

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
+require('dotenv').config()
+
 const Note = require('./models/note')
 
-
 let notes = [
-  {
-    id: 1,
-    content: "Pitäkää hauskaa mökillä",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Muistakaa uida ja saunoo",
-    important: false
-  },
 ]
+
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -26,10 +17,12 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 app.use(requestLogger)
-app.use(cors())
-app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -44,12 +37,6 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
 })
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -73,6 +60,7 @@ app.get('/api/notes/:id', (request, response) => {
     response.json(note)
   })
 })
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
